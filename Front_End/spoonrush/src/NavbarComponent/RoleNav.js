b4302a77f2d975e7950d84732e3f537a9dd21b0a
Ujjwal8271 <ujjwal.it.20183048@gmail.com>
2024-02-12 11:48:48 +0530

    add all previous data

---
+++ Front_End/spoonrush/src/NavbarComponent/RoleNav.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import NormalHeader from './NormalHeader';
+import DeliveryHeader from './DeliveryHeader';
+import AdminHeader from './AdminHeader';
+import UserHeader from './UserHeader';
+
+const RoleNav = () => {
+  const role = sessionStorage.getItem("role");
+   console.log(role);
+  if (role==="ROLE_CUSTOMER") {
+    return <UserHeader />;
+  } else if (role==="ROLE_MANAGER") {
+    return <AdminHeader />;
+  } else if (role==="ROLE_DELIVERY") {
+    return <DeliveryHeader/>;
+  } else {
+    return <NormalHeader/>;
+  }
+  
+};
+
+export default RoleNav
---
+++ Front_End/spoonrush/src/PageComponents/HomePage.js
@@ -0,0 +1,98 @@
+
+import React, { useEffect, useState } from 'react'
+import { Await, useParams } from 'react-router-dom'
+import Carousel from './Carousel';
+import Footer from '../NavbarComponent/Footer';
+import axios from 'axios';
+
+function HomePage() {
+
+  let response;
+    //get user id from url
+     const { categoryId , categoryName} =useParams();
+    
+     const [foods , setFoods]=useState([]);
+     const [searchText ,setSearchText] =useState("");
+     const [tempSearchText ,setTempSearchText]=useState("");
+
+  useEffect(()=>{
+    const fetchData = async () => {
+      try {
+        if (categoryId == null && searchText === "") {
+          // fetch all food
+        response = await axios.get(
+            `http://localhost:8080/food/fetch/all`
+          );
+        } else if (searchText) {
+          // fetch foods by name
+          response = await axios.get(
+            `http://localhost:8080/food/search?foodName=${searchText}`
+          );
+        } else {
+          // fetch foods by category
+          response = await axios.get(
+            `http://localhost:8080/food/fetch/category-wise?categoryId=${categoryId}`
+          );
+        }
+        if (response.data) {
+          setFoods(response.data.foods);
+        }
+
+      }catch(error){
+        console.error("Error fetch",error);
+      }
+    };
+     fetchData();
+  },[categoryId ,searchText]);
+
+  const searchFoods = (e) => {
+    e.preventDefault();
+    setSearchText(tempSearchText);
+  };
+
+
+  return (
+    <div className="container-fluid mb-2">
+        <Carousel/> 
+      <div className="d-flex aligns-items-center justify-content-center mt-5">
+        <form class="row g-3">
+          <div class="col-auto">
+            <input
+              type="text"
+              class="form-control"
+              id="inputPassword2"
+              placeholder="Enter Food Name..."
+              onChange={(e) => setTempSearchText(e.target.value)}
+              style={{
+                width: "350px",
+              }}
+              value={tempSearchText}
+              required
+            />
+          </div>
+          <div class="col-auto">
+            <button
+              type="submit"
+              class="btn bg-color custom-bg-text mb-3"
+              onClick={searchFoods}
+            >
+              Search
+            </button>
+          </div>
+        </form>
+      </div>
+
+      <div className="col-md-12 mt-3 mb-5">
+        <div className="row row-cols-1 row-cols-md-4 g-4">
+          {foods.map((food) => {
+           //food card detail maped
+          })}
+        </div>
+      </div>
+      <hr />
+     <Footer/>
+    </div>
+  )
+}
+
+export default HomePage
